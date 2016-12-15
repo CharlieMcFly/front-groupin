4ab12b4ba94676b69350bed3e95d0a01818f54c5
Charlie Quetstroey <cquetstroey@hotmail.fr>
2016-12-15 22:20:34 +0100

    feat(ajoutamis) : Fonctionnement du modal pour l'ajout d'amis

--- app/partial/modal-add-friend.controller.js
+++ app/partial/modal-add-friend.controller.js
@@ -8,10 +8,47 @@
         .module('app')
         .controller('modalAjoutFriendController', modalAjoutFriendController);
 
-    modalAjoutFriendController.$inject = [];
+    modalAjoutFriendController.$inject = ['Users', 'User', '$uibModalInstance'] ;
 
-    function modalAjoutFriendController () {
+    function modalAjoutFriendController (Users, User, $uibModalInstance) {
+
+        vm = this;
+
         var vm = this;
+        var friends = User.getFriends();
+        var user = User.getUser();
+        var users = Users.getAllUsers();
+
+        vm.users = [];
+        if(users.data != undefined){
+            Object.keys(users.data).forEach(function(key) {
+                if(friends.data != undefined){
+                    if(friends.data[key] == undefined && key != user.uid)
+                        vm.users.push(users.data[key]);
+                }else{
+                    if(key != user.uid)
+                        vm.users.push(users.data[key]);
+                }
+            });
+        }
+
+        var userToAdd = [];
+        vm.selectionAmis = function(user){
+            if(userToAdd.indexOf(user) < 0)
+                userToAdd.push(user);
+            else{
+               var pos = userToAdd.indexOf(user);
+               userToAdd.splice(pos,1);
+            }
+        }
+
+        vm.cancel = function(){
+            $uibModalInstance.dismiss();
+        }
+
+        vm.ajouter = function(){
+            $uibModalInstance.close(userToAdd);
+        }
 
     }
 })();
