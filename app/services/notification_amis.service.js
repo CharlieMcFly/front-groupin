0312d261d08030c665065637dd1b0cc1e8f37818
Charlie Quetstroey <cquetstroey@hotmail.fr>
2016-12-15 22:25:59 +0100

    feat(ajoutamis) : Ajout des service pour l'ajout d'ami

---
+++ app/services/notification_amis.service.js
@@ -0,0 +1,20 @@
+/**
+ * Created by charlie on 14/12/16.
+ */
+(function() {
+    'use strict';
+    angular
+        .module('app')
+        .factory('NotifsAmisService', NotifsAmisService);
+
+    NotifsAmisService.$injection = ['$resource'];
+
+    function NotifsAmisService($resource) {
+
+        var localhost = "http://localhost:8080/";
+        var dev = "https://platine-groupin.herokuapp.com/";
+
+        return $resource(localhost + 'notifications_amis/:uid');
+
+    }
+})();
