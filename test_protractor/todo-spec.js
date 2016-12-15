11da27a65ff25b8f3caf2b4700131822b14f86b3
Charlie Quetstroey <cquetstroey@hotmail.fr>
2016-12-15 23:41:55 +0100

    test(webdriver) : config

---
+++ test_protractor/todo-spec.js
@@ -0,0 +1,13 @@
+/**
+ * Created by charlie on 15/12/16.
+ */
+describe('TEST WEBDRIVER', function() {
+    beforeEach(function() {
+        browser.get('https://groupin-146311.firebaseapp.com/');
+    });
+
+    it('should have a title', function() {
+
+    });
+});
+
