7f98e71e4b41ddefbcca4564a189d8cec4a7bebc
CharlieMcFly <cquetstroey@hotmail.fr>
2017-01-20 15:21:45 +0100

    fix(connection) : localhost to dev

--- app/app.module.js
+++ app/app.module.js
@@ -21,7 +21,7 @@
 
         ])
         .constant("mode", {
-                "dev" : "http://localhost:8080/",
-                "prod" : "https://platine-groupin.herokuapp.com/"
+                "prod" : "http://localhost:8080/",
+                "dev" : "https://platine-groupin.herokuapp.com/"
         });
 })();
